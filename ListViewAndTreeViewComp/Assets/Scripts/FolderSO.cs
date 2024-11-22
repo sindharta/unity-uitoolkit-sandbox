454350d5cfcc5fe0862819910d76a6a51cb750ca
sin <sindharta.tanuwijaya@unity3d.com>
2024-11-22 18:10:45 +0900

    add setter

--- ListViewAndTreeViewComp/Assets/Scripts/FolderSO.cs
+++ ListViewAndTreeViewComp/Assets/Scripts/FolderSO.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 
 [Serializable]
 public class FolderSO : ScriptableObject {
+    public void SetFolder(Folder folder) => m_folder = folder;
+    
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------    
     [SerializeField] private Folder m_folder;
 }
